3f51827f67937d4b76a83c57cbdfce2c1e9f5424
Vo Do Quang Duong <quangduongaptx4869@gmail.com>
2025-10-03 02:31:44 +0700

    add chat page

--- fe/src/layouts/MainLayout.tsx
+++ fe/src/layouts/MainLayout.tsx
@@ -1,13 +1,13 @@
-import Footer from "@/components/Footer";
+// import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React from "react";
 import { Outlet } from "react-router";
 
 export default function MainLayout() {
   return (
-    <div className="text-[1vw]">
+    <div className="text-[1vw] min-h-0">
       <Header />
-      <div className="mt-[3vw]">
+      <div className="mt-[3vw] min-h-0">
         <Outlet />
       </div>
       {/* <Footer /> */}
